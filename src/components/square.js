5bf2d4700bdc61f4fc6aa07b44df72722f1597b7
BlakeAllred <blake.allred95@icloud.com>
2020-09-01 13:17:15 -0600

    added swuare component

---
+++ src/components/square.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react' 
+
+export default class Square extends Component {
+   constructor(props) {
+       super(props)
+
+       this.state = {
+           
+       }
+   }
+
+render() {
+       return (
+           <div className = 'square'>
+               
+           </div>
+       )
+   }
+}
